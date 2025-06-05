c74faba630f0f6e3d53a2ce4ae77d195c38608d5
root <root@master01.server.vm>
2025-06-05 19:02:12 +0530

    add functions

--- kubeadm_installation/Kubeadm_Common_fedora.sh
+++ kubeadm_installation/Kubeadm_Common_fedora.sh
@@ -2,7 +2,7 @@
 # Author: Mahin Raza
 # Date: 2025-6-1
 # Description: This script sets up a Kubernetes cluster using kubeadm on Fedora-based systems.
-# Version: 0.0.8
+# Version: 1.0.0
 
 ###############################################################################################
 # USAGE:
@@ -28,22 +28,24 @@
 
 # Common Script that execute on Both "Master" & "Worker" Nodes:
 
+# Check if the IP address is valid
 ip_Validate() {
     local ip_address="$1"
     echo "Validating IP address: $ip_address"
-    # Check if the IP address is valid
+    sleep 2
     if ! [[ "$ip_address" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Invalid IP address format."
+        echo "IP address $ip_address is not valid. Please enter a valid IPv4 address."
         exit 1
     else
         echo "IP address $ip_address is valid."
     fi
 }
 
+# Check if the IP address is reachable
 ping_test() {
     local ip_address="$1"
     echo "Pinging IP address: $ip_address"
-    # Check if the IP address is reachable
+    sleep 2
     if ! ping -c 1 "$ip_address" &> /dev/null; then
         echo "The IP address $ip_address is not reachable. Please check the network connection."
         exit 1
@@ -52,6 +54,21 @@ ping_test() {
     fi
 }
 
+# Check if the IP address is already in /etc/hosts  
+# and add it if not present
+# This function will add the hostname and IP address to /etc/hosts if it does not already exist
+Validate_hosts {
+    local hostname="$1"
+    local ip_address="$2"   
+    if ! grep -q "$ip_address $hostname.$domain_name $hostname" /etc/hosts; then
+        echo "$ip_address $hostname.$domain_name $hostname" | sudo tee -a /etc/hosts > /dev/null
+    else
+        echo "Entry already exists in /etc/hosts. Skipping..."
+    fi
+}
+
+# Function to input nodes details
+# This function will prompt the user to enter the details of the nodes (hostname and IP address)    
 input_nodes() {
     Node_Type="$1"
     echo "Please enter the initial $Node_Type nodes details:"
@@ -62,13 +79,18 @@ input_nodes() {
             break
         fi
         read -p "Enter the $Node_Type-node-$count IP address: " ip_address
-        
-        # Validate IP address format
+
+        # Validate inputs
+        if [[ -z "$hostname" ]] && [[ -z "$ip_address" ]]; then
+            echo "Hostname and IP address cannot be empty. Please try again."
+            continue
+        fi
+        sleep 1
         ip_Validate "$ip_address"
-        ping_test "$ip_address"
-        
-        # Add node to /etc/hosts
-        echo "$ip_address $hostname.$domain_name $hostname" | sudo tee -a /etc/hosts > /dev/null
+        sleep 1
+        ping_test "$ip_address"    
+        sleep 1        
+        Validate_hosts "$hostname" "$ip_address"
         ((count++))
     done
 }
@@ -79,31 +101,31 @@ pre_install() {
     local ip_address
 
     read -p "Enter the domain name for the node (e.g., server.vm): " domain_name
-    read -p "Enter the hostname for the node (e.g., master1/worker1): " hostname
+    read -p "Enter the hostname for the node (e.g., master01/worker01): " hostname
     read -p "Enter the IP address for the node: " ip_address
 
+    if [ -z "$domain_name" ]; then
+        domain_name="server.vm"  # Default domain name
+    fi
+  
     # Validate inputs
-    if [[ -z "$domain_name" || -z "$hostname" || -z "$ip_address" ]]; then
-        echo "Domain name, hostname, and IP address cannot be empty."
+    if [[ -z "$hostname" ]] && [[ -z "$ip_address" ]]; then
+        echo "Hostname and IP address cannot be empty."
         exit 1
     fi
-    sleep 2
+
+    sleep 1
+
     # Example usage or returning the values
     echo "Domain: $domain_name"
     echo "Hostname: $hostname"
     echo "IP Address: $ip_address"
 
-    sleep 2
-    
-    # Check if the IP address is valid
+    sleep 1
     ip_Validate "$ip_address"
-
-    sleep 2
-
+    sleep 1
     ping_test "$ip_address"
-
-    sleep 2
-
+    sleep 1
     hostnamectl set-hostname "$hostname.$domain_name"
 
     # Ensure we have permission to write to /etc/hosts
@@ -112,12 +134,7 @@ pre_install() {
         exit 1
     fi
 
-    # Check if the IP address is already in /etc/hosts     
-    if ! grep -q "$ip_address $hostname.$domain_name" /etc/hosts; then
-        echo "$ip_address $hostname.$domain_name $hostname" | sudo tee -a /etc/hosts > /dev/null
-    else
-        echo "Entry already exists in /etc/hosts. Skipping..."
-    fi
+    validate_hosts "$hostname" "$ip_address"
 
     if [[ $1 == "MASTER" ]];then             
         input_nodes "WORKER"  
@@ -202,7 +219,7 @@ sudo firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address
 
 if [[ "$NODE" == "MASTER" ]]; then
     echo "Configuring firewall rules for Master Node..."
-    for port in 6443 2379-2380 2380 10250 10259 10257 179 5473 80 443; do
+    for port in 6443 2379 2380 10250 10259 10257 179 5473 80 443 53; do
         sudo firewall-cmd --permanent --add-port=$port/tcp
     done
     for port in 5473 51820 4789 53 51821; do
@@ -211,7 +228,7 @@ if [[ "$NODE" == "MASTER" ]]; then
     firewall-cmd --reload
 else
     echo "Configuring firewall rules for Worker Node..."
-    for port in 10250 10256 179 5473 80 443 2379; do
+    for port in 10250 10255 10256 179 5473 80 443 2379 53; do
         sudo firewall-cmd --permanent --add-port=$port/tcp
     done
     for port in 4789 51820 51821 53 2379; do
